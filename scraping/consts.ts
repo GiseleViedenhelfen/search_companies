14d88ff978f63a53a98b2a58c57edc680a949d26
Gisele <gisele.viedenhelfen@gmail.com>
2024-02-20 13:19:16 -0300

    go to the main  and add values to form

---
+++ scraping/consts.ts
@@ -0,0 +1,22 @@
+export const page_url = "https://casadosdados.com.br/solucao/cnpj/pesquisa-avancada"
+export const inputs = {
+  uf: {
+    tag: 'input.input.is-is-normal[placeholder="Selecione o estado"]',
+    value: 'Rio Grande do Sul'
+  },
+  city: {
+    tag: 'input.input.is-normal[placeholder="Selecione um município"]',
+    value: 'Porto Alegre'
+  },
+  date: {
+    tag: 'input.input[placeholder="A partir de"]',
+    value: '19/02/2024'
+  }
+}
+export const mei = 'span.check.is-default'
+export const email = 'span.check.is-warning';
+export const successButton = 'a.button.is-success.is-medium';
+export const formButtons = {
+  mei: 'span.check.is-default',
+  email: 'span.check.is-warning'
+}
