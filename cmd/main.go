4ace61eaf95928589c0ab681b654b6f28dba0368
Gisele <gisele.viedenhelfen@gmail.com>
2024-02-22 14:43:10 -0300

    call the reader and insert functions before the api itself

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "github.com/GiseleViedenhelfen/search_companies/pkg/routes"
+import (
+	"log"
+
+	jsonreader "github.com/GiseleViedenhelfen/search_companies/pkg/json_reader"
+	"github.com/GiseleViedenhelfen/search_companies/pkg/routes"
+)
 
 func main() {
+	data, err := jsonreader.Reader()
+	if err != nil {	
+		log.Fatalln("Some error occured getting data from json file")
+	}
+	jsonreader.InsertData(data)
 	routes.ApiRunner()
 }
