a5028934ec7690d2823d2cce228ba9e4cb24ef6c
Gisele <gisele.viedenhelfen@gmail.com>
2024-02-22 11:11:16 -0300

    add file with functions to connect to puppeteer, get the current date and add a delay

---
+++ scraping/helpers.ts
@@ -0,0 +1,44 @@
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
+import puppeteer from "puppeteer-extra";
+import { Browser, Page } from "puppeteer";
+
+export interface Company {
+  cnpj: string;
+  corporate_name: string;
+  registered_capital: string;
+  legal_form: string;
+  phone: string;
+  mail: string;
+  main_activity: string;
+}
+export async function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export async function connect(): Promise<Page> {
+  puppeteer.use(StealthPlugin());
+
+  const browser: Browser = await puppeteer.launch({
+    headless: false,
+    executablePath: puppeteer.executablePath(),
+  });
+
+  const page: Page = await browser.newPage();
+  return page;
+}
+export function getDate(): string {
+  const currentDate: Date = new Date();
+  const yesterdayDate: Date = new Date(currentDate);
+  yesterdayDate.setDate(currentDate.getDate() - 1);
+
+  const year: number = yesterdayDate.getFullYear() + 1;
+  const month: number = yesterdayDate.getMonth() + 1;
+  const day: number = yesterdayDate.getDate();
+
+  const formattedMonth: string = month < 10 ? `0${month}` : `${month}`;
+  const formattedDay: string = day < 10 ? `0${day}` : `${day}`;
+  const formattedDate: string = `${formattedDay}-${formattedMonth}-${year}`;
+  
+  return formattedDate;
+}
+
