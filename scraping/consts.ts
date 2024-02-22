a9dede536be644505187cd92ace3cec94e716418
Gisele <gisele.viedenhelfen@gmail.com>
2024-02-22 11:09:21 -0300

    remove unsed consts and add dinamic getDate function to date input field

--- scraping/consts.ts
+++ scraping/consts.ts
@@ -1,4 +1,7 @@
-export const page_url = "https://casadosdados.com.br/solucao/cnpj/pesquisa-avancada"
+import { getDate } from "./helpers";
+export const filePath = "../data.json";
+export const page_url = "https://casadosdados.com.br"
+export const root = page_url + "/solucao/cnpj/pesquisa-avancada"
 export const inputs = {
   uf: {
     tag: 'input.input.is-is-normal[placeholder="Selecione o estado"]',
@@ -10,13 +13,11 @@ export const inputs = {
   },
   date: {
     tag: 'input.input[placeholder="A partir de"]',
-    value: '19/02/2024'
+    value: getDate()
   }
 }
+export const keys = ['CNPJ', 'Razão Social', 'Natureza Jurídica', 'Capital Social', 'E-MAIL', 'Telefone', 'Atividade Principal'];
 export const mei = 'span.check.is-default'
 export const email = 'span.check.is-warning';
 export const successButton = 'a.button.is-success.is-medium';
-export const formButtons = {
-  mei: 'span.check.is-default',
-  email: 'span.check.is-warning'
-}
+export const lenButton = 'ul.pagination-list li:last-child';
