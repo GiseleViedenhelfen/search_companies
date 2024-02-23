664e2a1ee2f8949b2224b35cbe0fc9392e6461a7
Gisele <gisele.viedenhelfen@gmail.com>
2024-02-22 22:18:08 -0300

    adjust the json path to match container

--- pkg/json_reader/reader.go
+++ pkg/json_reader/reader.go
@@ -2,16 +2,24 @@ package jsonreader
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/GiseleViedenhelfen/search_companies/pkg/models"
 )
 
 func Reader()(companies []models.Company, err error) {
-	filePath := "./data.json"
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("Error getting current directory: %v", err)
+		return nil, err
+}
 
+	filePath := filepath.Join(dir, "data.json")
 	data, err := os.ReadFile(filePath)
+	fmt.Println(filePath)
 	if err != nil {
 		log.Fatalf("Error reading file: %v", err)
 		return
