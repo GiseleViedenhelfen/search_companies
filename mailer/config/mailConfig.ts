1d28a3aae37f3ebe4cead854eb5350fb38cd2aae
Gisele <gisele.viedenhelfen@gmail.com>
2024-02-29 13:42:10 -0300

    add a file with functions and consts there are important to nodemailer

---
+++ mailer/config/mailConfig.ts
@@ -0,0 +1,32 @@
+import { configDotenv } from "dotenv";
+import path from "path";
+const file = path.resolve(__dirname, "../../img/logo.png");
+configDotenv;
+const { SENDER, SENDER_PASS, HOST } = process.env;
+export const transporterConfig = {
+  host: HOST,
+  port: 465,
+  secure: true,
+  auth: {
+    user: SENDER,
+    pass: SENDER_PASS,
+  },
+  socketTimeout: 10 * 1000,
+  connectionTimeout: 10 * 1000,
+};
+export const imageAttachment = {
+  filename: "logo.png",
+  path: file,
+  cid: "unique@image",
+};
+export function options (mail: string, content: string) {
+  const options = {
+    from: SENDER,
+    to: mail,
+    subject: 'Emita ou renove seu certificado digital',
+    html: content,
+    attachments: [imageAttachment],
+
+  }
+  return options
+}
