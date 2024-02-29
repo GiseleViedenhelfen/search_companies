904799df8fe82d6790dab0f3f2a6b1997fb15082
Gisele <gisele.viedenhelfen@gmail.com>
2024-02-29 13:45:37 -0300

    request from api the not sent companies and the endpoint to edit the mails_sent

---
+++ mailer/api/request.ts
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { configDotenv } from "dotenv";
+configDotenv
+const { PORT } = process.env;
+const URL = `http://api:${PORT}`
+export interface Company {
+  id: number;
+  mail: string;
+  cnpj: string;
+  subscribed: number,
+  mails_sent: number,
+  corporate_name: string;
+  main_activity: string;
+  legal_form: string;
+  phone: string;
+  registered_capital: string;
+}
+export async function fetchCompanies(): Promise<Company[] | null> {
+  try {
+    const response = await axios({
+      method: 'GET',
+      url: `${URL}/sent`,
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error:', error);
+    return null
+  }
+};
+export async function editMailSent(data: Company) {
+  try {
+    const response = await axios({
+      method: 'PATCH',
+      url: `${URL}/sent/${data.id}`,
+      data: data,
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
