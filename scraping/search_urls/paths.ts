cc858c6f8566afcccea1ce989af3525f146460fb
Gisele <gisele.viedenhelfen@gmail.com>
2024-02-29 20:20:08 -0300

    fix the loop to go to the right number of pages

--- scraping/search_urls/paths.ts
+++ scraping/search_urls/paths.ts
@@ -1,23 +1,27 @@
 import { Page } from "puppeteer";
 import { delay } from "../helpers";
-import {urlsPerPage, nextPage} from "../functions/paths"
+import { urlsPerPage, nextPage } from "../functions/paths";
 
 export default async function (page: Page, range: number): Promise<string[] | null> {
   const urlContainer: string[] = [];
-  const urls = await urlsPerPage(page);
-  if (urls === null) {
-    return null
-  }
   if (range === 0) {
-      urlContainer.push(...urls);
+    await extractUrls(page, urlContainer)
+  } else {
+    while (range >= 0) {
+      await extractUrls(page, urlContainer)
+      await nextPage(page);
+      await delay(2000);
+      range--;
     }
-  for (let i = 0; i < range - 1; i++) {  
-      urlContainer.push(...urls);
-    await nextPage(page);
-    await delay(2000);
   }
 
   return urlContainer;
 }
 
-
+async function extractUrls(page:Page, container : string[]) {
+  const urls = await urlsPerPage(page);
+  if (urls === null) {
+    return null;
+  }
+  container.push(...urls);
+}
