6fa7a27685527632a38723a6185d265bdafc27f2
Gisele <gisele.viedenhelfen@gmail.com>
2024-04-08 16:05:43 -0300

    remove database creation from init.go

--- api/pkg/routes/init.go
+++ api/pkg/routes/init.go
@@ -5,8 +5,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-
-	db "github.com/GiseleViedenhelfen/search_companies/pkg/db"
 	"github.com/joho/godotenv"
 )
 
@@ -15,13 +13,8 @@ func ApiRunner() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("Some error occured while getting .env file. Err: %s", err)
-		
 	}
 	p := os.Getenv("PORT")
-	err = db.CreateInitDB()
-	if err != nil {
-		log.Fatalf("Some error occured while creating database. Err: %s", err)
-	}
 	router := http.NewServeMux()
 	HandleRoutes(router)
 
@@ -29,7 +22,6 @@ func ApiRunner() {
 		Addr:    fmt.Sprintf(":%v", p),
 		Handler: router,
 	}
-	
 	log.Printf("Server starting on port %v\n", p)
 	log.Fatal(server.ListenAndServe())
 }
