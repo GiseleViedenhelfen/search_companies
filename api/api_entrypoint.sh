4c6f4767a30b5be39504bab816e6d7b31a1dfd0d
Gisele <gisele.viedenhelfen@gmail.com>
2024-04-08 15:58:18 -0300

    improve database container management for api

--- api/api_entrypoint.sh
+++ api/api_entrypoint.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 cd api/
 go mod tidy
-if docker ps | grep -q "scraping_database"; then
-    echo "Container 'scraping_database' is already running."
-else
 echo "Starting MySQL database container..."
-source "$(dirname "$0")/.env"
-docker run --name scraping_database -e MYSQL_USER=${DB_USER} -e MYSQL_PASSWORD=${DB_PASSWORD} -e MYSQL_ROOT_PASSWORD=${DB_ROOTPASS} -e MYSQL_DATABASE=${DBNAME}  -p ${DB_PORT}:3307 -d mysql --port=${DB_PORT}
-# Run Go application
-while ! docker exec scraping_database mysqladmin ping -h"172.17.0.2"  -P"3307" --silent; do
-    sleep 1
-done
-echo "MySQL database is ready."
+
+source ".env"
+if docker ps -a --format '{{.Names}}' | grep -q "api_database"; then
+    if docker inspect -f '{{.State.Running}}' "api_database" 2>/dev/null | grep -q "true"; then
+        echo "Container 'api_database' is already running."
+    else
+        echo "Starting container 'api_database'..."
+        docker start "api_database"
+        while ! docker exec api_database mysqladmin ping -h"172.17.0.2" -P"3307" --silent; do
+            sleep 1
+        done
+        echo "Container 'api_database' started."
+    fi
+else
+    echo "Starting a new container 'api_database'..."
+    docker run --name api_database -e MYSQL_USER=${DB_USER} -e MYSQL_PASSWORD=${DB_PASSWORD} -e MYSQL_ROOT_PASSWORD=${DB_ROOTPASS} -e MYSQL_DATABASE=${DBNAME} -p ${DB_PORT}:3307 -d mysql --port=${DB_PORT}
+    while ! docker exec api_database mysqladmin ping -h"172.17.0.2" -P"3307" --silent; do
+        sleep 1
+    done
+    echo "New container 'api_database' started."
 fi
 echo "Running Go application..."
 go run cmd/main.go
