90a12c285532f1c2d8575be06d8bd8dff6241be7
Gisele <gisele.viedenhelfen@gmail.com>
2024-04-08 15:59:51 -0300

    mailer container starts process as soon as api is running

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-
-# Execute scraping tasks
 chmod +x "$0"
 echo "Executing scraping tasks..."
 ./scraping/scraping_entrypoint.sh
 
-# Execute Go tasks
 echo "Executing api tasks..."
-./api/api_entrypoint.sh
-
-# Execute mailer tasks
-echo "Executing mailer tasks..."
+source "./api/.env"
+./api/api_entrypoint.sh &
+while ! nc -z localhost ${PORT}; do
+    sleep 1
+done
+echo "API is running. Starting mailer service..."
 ./mailer/mailer_entrypoint.sh
+echo "Mailer service completed. Killing process on api port ..."
+sudo lsof -ti:${PORT} | xargs sudo kill
